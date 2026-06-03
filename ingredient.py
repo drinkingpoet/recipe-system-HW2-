84cef58ad5859eb936695b53c891864c70f3ada2
Абрамин Артем <artdmiabramin@edu.hse.ru>
2026-06-03 21:34:49 +0300

    ингредиент и рецепт готовы

--- ingredient.py
+++ ingredient.py
@@ -1,1 +1,36 @@
+#класс под ингредиент
 
+class Ingredient:
+    def __init__(self, name, quantity, unit):
+        self.name = name
+        self._quantity = None
+        self.quantity = quantity
+        self.unit = unit
+
+    @property
+    def quantity(self):
+        return self._quantity
+    
+    @quantity.setter
+    def quantity(self, value) -> None:
+        try:
+            value = float(value)
+        except(TypeError, ValueError):
+            raise ValueError("количество должно быть числом")
+        if value <= 0:
+            raise ValueError("количество должно быть положительным")
+        self._quantity = value
+
+    def __str__(self):
+        return f"{self.name}: {self.quantity} {self.unit}"
+    
+    def __repr__(self):
+        return f"ingredient('{self.name}', {self.quantity}, '{self.unit}')"
+    
+    def __eq__(self, other):
+        if not isinstance(other, Ingredient):
+            return NotImplemented
+        return self.name == other.name and self.unit == other.unit
+    #для работы со словарем
+    def __hash__(self):
+        return hash((self.name, self.unit))
--- recipe.py
+++ recipe.py
@@ -1,1 +1,47 @@
+from typing import List, Optional
+from ingredient import Ingredient
 
+#сам рецепт блюда
+class Recipe:
+    def __init__(self, title, ingredients):
+        self.title = title
+        if ingredients is not None:
+            self._ingredients = ingredients
+        else:
+            self._ingredients = []
+    
+    @property
+    def ingredients(self):
+        return self._ingredients.copy()
+    
+    def add_ingredient(self, ingredient) -> None:
+        for ingred in self._ingredients:
+            if ingred == ingredient:
+                ingred.quantity += ingredient.quantity
+                return
+        self._ingredients.append(ingredient)
+    
+    @staticmethod
+    def is_ratio_valid(ratio):
+        try:
+            return (float(ratio) > 0)
+        except(TypeError, ValueError):
+            return False
+    
+    def scale(self, ratio):
+        if not self.is_ratio_valid(ratio):
+            raise ValueError("коэффициент должен быть положительным числом")
+        scaled = []
+        for ingred in self._ingredients:
+            new_quantity = ingred.quantity * ratio
+            new_ingredient = Ingredient(ingred.name, new_quantity, ingred.unit)
+            scaled.append(new_ingredient)
+        return Recipe(self.title, scaled)
+    
+    #для количества
+    def __len__(self):
+        return len(self._ingredients)
+    
+    def __str__(self) :
+        s = "\n".join(f"  - {ingred}" for ingred in self._ingredients)
+        return f"{self.title}:\n{s}"
